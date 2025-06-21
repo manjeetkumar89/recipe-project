7a01416f03ecb1fbb1b5287fc4385ef8cb48d677
manjeetkumar89 <scit403@gmail.com>
2025-06-21 14:33:30 +0530

    images changed

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -32,7 +32,7 @@ const About = () => {
         {/* Image */}
         <div className="rounded-2xl overflow-hidden shadow-md">
           <img
-            src="./../public/aboutrecipe.avif"
+            src="https://images.unsplash.com/photo-1495546968767-f0573cca821e?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="About TasteBuds"
             className="object-cover w-full h-full"
           />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,7 +58,7 @@ const Home = () => {
           <div className="relative">
             <div className="rounded-3xl overflow-hidden shadow-xl border border-gray-100">
               <img
-                src="./../public/homerecipe.avif"
+                src="https://images.unsplash.com/photo-1490645935967-10de6ba17061?q=80&w=1153&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
                 alt="Delicious Dish"
                 className="object-cover w-full h-full"
               />
